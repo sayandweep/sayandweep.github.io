78ef1f9af4cec14c41c5005ee582b772c48672bc
Sayandweep <81100142+sayandweep@users.noreply.github.com>
2024-10-27 00:42:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+function chngPg() {
+    window.location = 'portfolio.html'
+}
+
+//  animation scrolling
+window.onscroll = function() {
+    var nav = document.getElementById('details');
+    if ( window.pageYOffset > 50 ) {
+        nav.classList.add("details1");
+        nav.classList.remove("fullscreenback");
+    }
+    if ( window.pageYOffset < 60 ) {
+        nav.classList.add("fullscreenback");
+    }
+}
